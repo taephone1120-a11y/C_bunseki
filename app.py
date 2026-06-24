feab8ff717630f057194f3149246f6edb79b5878
taephone1120-a11y <taephone1120@gmail.com>
2026-06-24 15:23:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -521,7 +521,7 @@ if st.session_state.raw_data:
     if filter_rev_min is not None: query_df = query_df[query_df["_rev_num"] >= filter_rev_min]
     if filter_rev_max is not None: query_df = query_df[query_df["_rev_num"] <= filter_rev_max]
     
-     def check_sales3_date_range(date_str):
+    def check_sales3_date_range(date_str):
     # 開始も終了も「未指定(None)」なら、文字データ（3ヶ月以上前など）も含めてすべて表示する
     if filter_sales3_min is None and filter_sales3_max is None: return True
     # どちらかが指定されている場合は、日付以外の文字データは除外する
