f35759a8ae52cff441bc5566daa15aa991d12a79
taephone1120-a11y <taephone1120@gmail.com>
2026-07-11 16:53:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ if mode == "キーワード検索":
 else:
     target_url = st.sidebar.text_input("🔗 Creemaの一覧URLを入力", value="")
 
-max_items = st.sidebar.number_input("🔢 取得する商品件数", min_value=1, max_value=1000, value=10, step=10)
+max_items = st.sidebar.number_input("🔢 取得する商品件数", min_value=1, max_value=10, value=10, step=10)
 
 # 取得速度は「高速（おすすめ）」に固定（画面には表示しない）
 speed_mode = "高速（おすすめ）"
